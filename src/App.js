45467a9990637e40bf88837a0efb55baaca5c026
Petronella Ndlovu <152957733+PetronellaNdlovu@users.noreply.github.com>
2024-06-30 19:20:23 +0200

    NAME CHANGE

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Matt Delac
+            Petronella Ndlovu
           </a>{" "}
           and is{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a City.."
+              className="form-control"
+            />
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </div>
+      </form>
       <h1>New York</h1>
       <ul>
         <li>Wednesday 07:00</li>
